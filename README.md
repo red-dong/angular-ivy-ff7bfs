74adf615e91fb0096da3c7ffae8e5b48a26932a3
red-dong <112997814+red-dong@users.noreply.github.com>
2022-09-08 13:48:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ff7bfs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ff7bfs)
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+ 
+public refMsg:string;
+constructor(){}
+ngOnInit():void{}
+getRefMsg(msg:string){
+  this.refMsg = msg;
+}
+}
